0f4fc7d38088fd917627303e62b31c9726d65379
alvarotellez <alvarotellez@ALVAROTELLE9EAA>
2016-11-14 18:32:33 +0100

    Falta adptive trigger

---
+++ ejercicio2-DI-Binding/ViewModels/clsMainPageVM.cs
@@ -0,0 +1,53 @@
+﻿using ejercicio2_DI_Binding.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2_DI_Binding.ViewModels
+{
+    class clsMainPageVM : INotifyPropertyChanged
+    {
+        private clsPersona _personaSeleccionada;
+        private ObservableCollection<clsPersona> _listado;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public clsMainPageVM()
+        {
+            clsListado lista = new clsListado();
+            _listado = lista.getListado();
+        }
+
+        public clsPersona personaSeleccionada
+        {
+            get
+            {
+                return _personaSeleccionada;
+            }
+            set
+            {
+                _personaSeleccionada = value;
+                OnPropertyChanged("personaSeleccionada");
+            }
+        }
+        public ObservableCollection<clsPersona> listado
+        {
+            get
+            {
+                return _listado;
+            }
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
